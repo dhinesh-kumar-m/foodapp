4c7c1c812786e1419f98f1388b9acae5ca97c344
Dhinesh <dhineshkumar@testpress.in>
2022-06-24 11:24:09 +0530

    Register restaurant and menu model

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from restaurant.models import Dish
+from restaurant.models import Menu
+from restaurant.models import Restaurant
 
 # Register your models here.
+
+
+class DishInline(admin.StackedInline):
+    model = Dish
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = [
+        "get_restaurant",
+        "name",
+    ]
+    search_fields = ["get_restaurant", "name"]
+    inlines = [DishInline]
+
+    @admin.display(description="Restaurant Name", ordering="restaurant__name")
+    def get_restaurant(self, obj):
+        return obj.restaurant.name
+
+
+@admin.register(Restaurant)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["title", "created"]
+    list_filter = [
+        "created",
+    ]
+    search_fields = ["title", "overview"]

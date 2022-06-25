ed0fbdb079da3a1ee9064a21f22f70f80d3edf56
Dhinesh <dinesh.k131997@gmail.com>
2022-06-25 17:43:55 +0530

    Add restaurant list view

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "restaurant"
+
 urlpatterns = [
-    path("", views.index),
+    path("", views.restaurant_list, name="restaurant_list"),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,13 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+
+from .models import Restaurant
 
 # Create your views here.
 def index(request):
     return render(request, "restaurant/list.html")
+
+
+def restaurant_list(request):
+    restaurants = Restaurant.objects.all()
+    return render(request, "restaurant/list.html", {"restaurants": restaurants})

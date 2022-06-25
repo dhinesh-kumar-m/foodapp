5b5e64596c2bcb99b70a7955e1438b5ddae53511
Dhinesh <dinesh.k131997@gmail.com>
2022-06-25 22:52:04 +0530

    Add sort filter and bookmark, visitor page

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -6,4 +6,5 @@ app_name = "restaurant"
 
 urlpatterns = [
     path("", views.restaurant_list, name="restaurant_list"),
+    path("<slug:list>", views.restaurant_list, name="user_list"),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
 from .models import Restaurant
 
@@ -8,6 +10,16 @@ def index(request):
     return render(request, "restaurant/list.html")
 
 
-def restaurant_list(request):
-    restaurants = Restaurant.objects.all()
-    return render(request, "restaurant/list.html", {"restaurants": restaurants})
+def restaurant_list(request, list=None):
+    sort = request.GET.get("order", "id")
+    restaurants = Restaurant.objects.order_by(sort)
+    user = User.objects.latest("id")
+
+    if list == "bookmark":
+        restaurants = restaurants.filter(users_bookmark=user)
+    elif list == "visited":
+        restaurants = restaurants.filter(users_visit=user)
+
+    return render(
+        request, "restaurant/list.html", {"restaurants": restaurants, "list": list}
+    )

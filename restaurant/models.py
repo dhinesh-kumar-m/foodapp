4aa31467f2a2fc40c988c87db12802bf39196adf
Dhinesh <dhineshkumar@testpress.in>
2022-06-27 12:12:05 +0530

    Add profile model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator
 from django.core.validators import MinValueValidator
@@ -92,3 +93,11 @@ class Dish(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     type = models.CharField(max_length=10, choices=FOOD_TYPE_CHOICES)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True)
+
+    def __str__(self):
+        return f"Profile for user {self.user.username}"

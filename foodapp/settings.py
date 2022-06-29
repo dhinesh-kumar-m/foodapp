c598946d9552bddf17bbf7a3f0c3fcde96230a69
Dhinesh <dhineshkumar@testpress.in>
2022-06-29 14:58:20 +0530

    Config settings

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "theme",
     "django_browser_reload",
     "django_filters",
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
@@ -147,8 +148,8 @@ STATIC_URL = "/static/"
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
-MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

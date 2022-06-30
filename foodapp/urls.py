5b2fad5ce973b8dad0fbe6a498e3219863241aa9
Dhinesh <dhineshkumar@testpress.in>
2022-06-30 12:00:38 +0530

    Change redirect_dashboard and remove existing

--- foodapp/urls.py
+++ foodapp/urls.py
@@ -19,11 +19,16 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include
 from django.urls import path
-from restaurant import views
 from django.views.generic import RedirectView
+from restaurant import views
 
 urlpatterns = [
-    path('',RedirectView.as_view(pattern_name='restaurant:restaurant_list', permanent=False)),
+    path(
+        "",
+        RedirectView.as_view(
+            pattern_name="restaurant:restaurant_list", permanent=False
+        ),
+    ),
     path("admin/", admin.site.urls),
     path("login", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
